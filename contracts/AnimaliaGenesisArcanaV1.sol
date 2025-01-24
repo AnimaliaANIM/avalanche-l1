e8c924d3e52e3e194c92db36f527b9c5710fddaa
Jeff <nightsilver@richmonkeys.com>
2025-01-24 21:46:48 +0800

    fix address send

--- contracts/AnimaliaGenesisArcanaV1.sol
+++ contracts/AnimaliaGenesisArcanaV1.sol
@@ -2,6 +2,7 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
